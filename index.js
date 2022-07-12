f70559aba4885f853f0b81301f088225572c4316
NikDoe <nikd0e@yandex.ru>
2022-07-12 16:36:58 +0300

    feature(cl-tools): callbacks-base solution for order files

--- index.js
+++ index.js
@@ -7,5 +7,23 @@ fs.readdir(process.cwd(), (error, filenames) => {
 		console.log(error);
 	}
 
-	console.log(filenames);
+	const allStats = Array(filenames.length).fill(null);
+
+	filenames.forEach((filename, index) => {
+		fs.lstat(filename, (err, stats) => {
+			if (err) {
+				console.log(err);
+			}
+
+			allStats[index] = stats;
+
+			const ready = allStats.every(stats => stats);
+
+			if (ready) {
+				allStats.forEach((stats, index) => {
+					console.log(filenames[index], stats.isFile());
+				});
+			}
+		});
+	});
 });
