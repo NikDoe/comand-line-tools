e31c821a3f98045eb5b76cc761ebd417b4f65d5a
NikDoe <nikd0e@yandex.ru>
2022-07-12 17:02:08 +0300

    feature(cl-tools): promisify-base solution for order files

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+// const util = require('util');
 
-fs.readdir(process.cwd(), (error, filenames) => {
+// Method #1
+const lstat = filename => {
+	return new Promise((res, rej) => {
+		fs.lstat(filename, (err, stats) => {
+			if (err) rej(err);
+			res(stats);
+		});
+	});
+};
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+// const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (error, filenames) => {
 	if (error) {
 		console.log(error);
 	}
 
-	const allStats = Array(filenames.length).fill(null);
-
-	filenames.forEach((filename, index) => {
-		fs.lstat(filename, (err, stats) => {
-			if (err) {
-				console.log(err);
-			}
-
-			allStats[index] = stats;
-
-			const ready = allStats.every(stats => stats);
-
-			if (ready) {
-				allStats.forEach((stats, index) => {
-					console.log(filenames[index], stats.isFile());
-				});
-			}
-		});
-	});
+	for (const filename of filenames) {
+		try {
+			const stats = await lstat(filename);
+			console.log(filename, stats.isFile());
+		} catch (err) {
+			console.log(err);
+		}
+	}
 });
